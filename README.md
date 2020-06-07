11b501babb61fd0d4cd8579ec687cb4dacca53ba
Pedro <pedro-javierf@users.noreply.github.com>
2020-06-07 20:19:57 +0200

    Minor commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MemWizard
  Advanced ROP and JOP gadget finder
+
+# Requirements
+ Memory Wizard uses the following libraries:
+ * Qt for Python
+ * Capstone
---
+++ src/MWengine/MWengine.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+from capstone import *
+
+
+
+class MemWizardEngine:
+    def __init__(self):
+        # Do something
+    
+    def dissasemble(self):
+        #creates a dissasembled version of a binary in memory
+
+    def searchROPGadget(self):
+        #search some rop gadgted
+
+    def searchJOPGadget(self):
+        #search some jop gadget
+
+    
